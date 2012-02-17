3c346cc7cc91e820ced307e6d8cc9c2eada36bce
Roman Birg <romanbirg@gmail.com>
2012-02-17 08:43:33 -0600

    remove Stk on crespo

--- full_crespo.mk
+++ full_crespo.mk
@@ -25,10 +25,7 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Camera
 PRODUCT_PACKAGES := \
     Camera
-    
-# Stk
-PRODUCT_PACKAGES := \
-    Stk
+
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
